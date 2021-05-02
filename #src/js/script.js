3852fc66eceb30e5308190a1cb086106ef90a471
Павел Климчук <pavlo95klymchuk@gmail.com>
2021-05-02 14:23:24 +0300

    fix bag

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 const menuBtn = document.querySelector(".header__button"),
   menu = document.querySelector(".nav");
-menuBtn.addEventListener("click", () => {
+  menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("header__button--active");
   menu.classList.toggle("header__nav--active");
 });
