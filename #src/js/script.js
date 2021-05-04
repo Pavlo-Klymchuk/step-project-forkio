39654d2672db728b099e92ffdce8b3a3c5fceb1e
ANNA GUDZENCO <makarovaanna90@gmail.com>
2021-05-04 17:02:31 +0300

    section rewiew done

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,7 +1,22 @@
 "use strict";
 const menuBtn = document.querySelector(".header__button"),
   menu = document.querySelector(".nav");
-  menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("header__button--active");
-  menu.classList.toggle("header__nav--active");
+
+menuBtn.addEventListener("click", () => {
+  if (menuBtn.classList.contains("header__button--active")) {
+    removeMenu();
+    document.addEventListener("click", (evt) => {
+      if (evt.target.tagName === "HEADER" || evt.target.tagName === "ARTICLE") {
+        removeMenu();
+      }
+    });
+  } else {
+    menuBtn.classList.add("header__button--active");
+    menu.classList.remove("header__nav--active");
+  }
 });
+
+function removeMenu() {
+  menuBtn.classList.remove("header__button--active");
+  menu.classList.add("header__nav--active");
+}
